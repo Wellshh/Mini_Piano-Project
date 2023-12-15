46e5bed408b449a50d871a0ea68742720580edbb
Wells <12212108@mail.sustech.edu.cn>
2023-12-15 11:29:29 +0800

    添加所有数字和字母数码管显示码表

---
+++ Mini_Piano.srcs/sources_1/new/Light_seg_Display.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/12/15 10:46:19
+// Design Name: 
+// Module Name: Light_seg_Display
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Light_seg_Display(
+
+    );
+endmodule
